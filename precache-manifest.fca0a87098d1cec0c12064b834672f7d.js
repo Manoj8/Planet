333987c8e6ed249893a170677b2d07659e51e2bc
Manoj Kumar <manoj08797@gmail.com>
2020-06-04 12:11:50 +0530

    Updates

---
+++ precache-manifest.fca0a87098d1cec0c12064b834672f7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6717e0de15988d363500",
+    "url": "/Planet/static/js/runtime~main.8e778b6d.js"
+  },
+  {
+    "revision": "d059425391c57a20aa27",
+    "url": "/Planet/static/js/main.c3a912c7.chunk.js"
+  },
+  {
+    "revision": "390a35ed0553c705cf7d",
+    "url": "/Planet/static/js/2.cd4bbc6d.chunk.js"
+  },
+  {
+    "revision": "d059425391c57a20aa27",
+    "url": "/Planet/static/css/main.af74f1fb.chunk.css"
+  },
+  {
+    "revision": "f4696ad65a96c4b1686fd23794d41e25",
+    "url": "/Planet/index.html"
+  }
+];
